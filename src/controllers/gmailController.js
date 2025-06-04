acf83d5b1f98b9da3013a32f0fa2a51a4898f0c3
Makesh R <makesh@5xtechnologies.in>
2025-06-04 14:51:22 +0530

    Fix gmail error

--- src/controllers/gmailController.js
+++ src/controllers/gmailController.js
@@ -1,5 +1,17 @@
 const { sendEmail } = require('../services/gmailService');
 const { generateEmailReply } = require('../services/openaiService');
+require('dotenv').config();
+const { google } = require('googleapis');
+
+// Gmail OAuth2
+const oAuth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    process.env.GOOGLE_REDIRECT_URI
+);
+oAuth2Client.setCredentials({ refresh_token: process.env.GOOGLE_REFRESH_TOKEN });
+
+const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
 
 async function handleGmailWebhook(req, res) {
     try {
